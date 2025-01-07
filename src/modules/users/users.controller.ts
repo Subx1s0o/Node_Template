d3fcb8c0d8fdc28ad436cbf0840e1687d03654bb
Subx1s0o <nazarsabardin@gmail.com>
2025-01-07 03:26:36 +0200

    fix: typing users module

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@/libs/decorators';
 import { Service } from 'typedi';
 import UsersService from './users.service';
+import { Request, Response } from 'express';
 
 @Service()
 @Controller('/users')
@@ -8,17 +9,17 @@ class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/')
-  async index(req: any, res: any) {
-    return res.send('index');
+  async index(req: Request, res: Response) {
+    return res.send(req.body);
   }
 
   @Get('/register')
-  async register(req: any, res: any) {
+  async register(req: Request, res: Response) {
     return await this.usersService.register(req, res);
   }
 
   @Get('/login')
-  async login(req: any, res: any) {
+  async login(req: Request, res: Response) {
     return await this.usersService.login(req, res);
   }
 }
